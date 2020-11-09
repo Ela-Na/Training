1ee099b98c6454e7b22ba8da2fff1a117a305a3f
Ela-Na <42371862+Ela-Na@users.noreply.github.com>
2020-11-08 20:16:17 -0500

    Add files via upload

---
+++ model_dispatcher.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 20:11:41 2020
+
+@author: Ela
+"""
+
+from sklearn import tree
+
+models = {
+        "decision_tree_gini": tree.DecisionTreeClassifier(
+                criterion="gini")'
+                "decision_tree_entropy":
+                    tree.DecisionTreeClassifier(criterion="entropy"),}
